9c3deb6b0d3d2a2e19fcee3cefdcf9417eadf5e7
RussianGun <34107490+RussianGun@users.noreply.github.com>
2019-01-20 14:23:13 +0300

    приора

--- ProfTest/app/models.py
+++ ProfTest/app/models.py
@@ -4,69 +4,74 @@ class admin(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	login = db.Column(db.String(20), index=True, unique=True)
 	password = db.Column(db.String(30))
-	first_name = db.Column(db.String(15), index=True, unique=True) 
-	surname = db.Column(db.String(15), index=True, unique=True)
-	second_name = db.Column(db.String(15), index=True, unique=True)
+	first_name = db.Column(db.String(15), index=True) 
+	surname = db.Column(db.String(15), index=True)
+	second_name = db.Column(db.String(15), index=True)
 
-	def 
+	def __repr__(self):
+        return '<admin {}>'.format(self.first_name) 
 
 
 class student(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	first_name = db.Column(db.String(15), index=True, unique=True)
+	first_name = db.Column(db.String(15), index=True)
 	surname = db.Column(db.String(15), index=True, unique=True)
-	second_name = db.Column(db.String(15), index=True, unique=True)
+	second_name = db.Column(db.String(15), index=True)
 	klass = db.Column(db.String(3))
 	birth_date = db.Column(db.Date)
 	phone_number = db.Column(db.String(12))
 
-	def
+	def __repr__(self):
+        return '<student {}>'.format(self.surname) 
 
 
 class result(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	student_id = db.Column(db.Integer)
-	test_id = db.Column(db.Integer)
+	student_id = db.Column(db.Integer, primary_key=True)
+	test_id = db.Column(db.Integer, primary_key=True)
 
-	def
-
+	def count(result_type)
+		
 
 class test(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String(100))
 	result_type = db.Column(db.Integer)
 
-	def
+	def __repr__(self):
+        return '<test {}>'.format(self.title) 
 
 
 class question(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	test_id = db.Column(db.Integer)
+	test_id = db.Column(db.Integer, primary_key=True)
 	text = db.Column(db.String(1000000))
 
-	def
+	def __repr__(self):
+        return '<question {}>'.format(self.text) 
 
 
 class student_answer(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	answer_id = db.Column(db.Integer)
-	test_id = db.Column(db.Integer)
-	student_id = db.Column(db.Integer)
-	result_id = db.Column(db.Integer)
-
-	def
+	answer_id = db.Column(db.Integer, primary_key=True)
+	test_id = db.Column(db.Integer, primary_key=True)
+	student_id = db.Column(db.Integer, primary_key=True)
+	result_id = db.Column(db.Integer, primary_key=True)
 
 
 class answer(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	question_id = db.Column(db.Integer)
+	question_id = db.Column(db.Integer, primary_key=True)
 	text = db.Column(db.String(1000000))
-	points = db.Column(db.Integer)
+	points = db.Column(db.Integer) 
 
-	def
-
-
-class result_type
+	def __repr__(self):
+        return '<answer {}>'.format(self.text) 
 
 
-class characters 
+class result_type:
+	pass
+
+
+class characters:
+	pass
